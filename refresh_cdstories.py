e75eb4be3b97e739a024cfc96c0ee0b4eee222bd
redlegoman <andy@mcdade.co.uk>
2021-11-10 14:52:00 +0000

    added cdstories

---
+++ refresh_cdstories.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+from plexapi.myplex import MyPlexAccount
+import sys
+
+#
+# this really simple script gets the items last added to each section,
+# checks if they are "artist" types, or in other words "music" and
+# then refreshes the metadata (reads the ID3 tags again).
+
+# account = MyPlexAccount('<email>', '<password>')
+# username and password found in ~/.config/plexapi/config.ini 
+# see: https://python-plexapi.readthedocs.io/en/latest/configuration.html
+try:
+  days=str(sys.argv[1])
+except:
+  days="1d"
+
+print(days)
+
+account = MyPlexAccount()
+plex = account.resource('plex').connect()  # returns a PlexServer instance
+
+
+for section in plex.library.sections():
+  print(section.title +"|"+section.type)
+  music = plex.library.section(section.title)
+  if section.title == "CD Stories":
+    if section.type == "artist":
+      #for item in music.searchAlbums(**{"addedAt>>": "20d"}):
+      for item in music.searchAlbums(**{"addedAt>>": days}):
+        print(item)
+        item.refresh()
+
