c775339435fbc72fcc92329b2f6b1eac7843229d
redlegoman <andy@mcdade.co.uk>
2024-11-04 10:22:01 +0000

    added library scans for just tidal

--- refresh.py
+++ refresh.py
@@ -15,10 +15,27 @@ try:
 except:
   days="1d"
 
+
 print(days)
 
 account = MyPlexAccount()
 plex = account.resource('plex').connect()  # returns a PlexServer instance
+plex_library = plex.library.section('Music: Tidal tmp')
+
+
+for lib in plex.library.sections():
+  for path in lib.locations:
+    if lib.uuid == "39725fb1-2c23-45ec-9277-a1b7c5b77f09" or lib.uuid == "f2497ca5-6d89-47e8-b1de-fd4763fa4e67":
+      print(lib.title +","+ lib.uuid)
+      print(path)
+      print(" -- scanning "+lib.title)
+      scan = lib.update()
+      print(scan)
+      print()
+      #scan = plex.library.librarysection.update(path)
+
+#exit()
+
 
 
 for section in plex.library.sections():
